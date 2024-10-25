23055e1739cb06cc04826924908b2941a8c656c1
Josiel Faria <developerjosiel@gmail.com>
2024-10-24 21:35:35 -0300

    add resources

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
+import { ProductsPage } from './pages/products/products.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: HomePage},
+  { path: 'pratas', component: ProductsPage},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomePage } from './pages/home/home.page';
+import { ProductsPage } from './pages/products/products.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePage,
+    ProductsPage
   ],
   imports: [
     BrowserModule,
