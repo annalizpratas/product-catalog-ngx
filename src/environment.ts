302629991925d0f3a91ae8bcc934c4817e6c8cae
Josiel Faria <developerjosiel@gmail.com>
2024-10-29 10:36:35 -0300

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://product-catalog-api-3lh0.onrender.com',
-  apiUrl: 'http://localhost:3030',
+  apiUrl: 'https://product-catalog-api-3lh0.onrender.com',
+  // apiUrl: 'http://localhost:3030',
 };
