588d2b25e9f259545090c77c03bc18971e4666c2
Josiel Faria <developerjosiel@gmail.com>
2024-10-28 21:24:11 -0300

    improves

--- src/app/components/banner-promo/banner-promo.component.ts
+++ src/app/components/banner-promo/banner-promo.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-banner-promo',
   templateUrl: './banner-promo.component.html',
-  styleUrls: ['./banner-promo.component.scss']
+  styleUrls: ['./banner-promo.component.scss'],
 })
-export class BannerPromoComponent {
+export class BannerPromoComponent implements OnInit {
+  @Input() size: string;
 
+  ngOnInit(): void {}
+
+  get bannerSize(): string {
+    return this.size === 'large' ? 'banner-large' : 'banner-small';
+  }
 }
